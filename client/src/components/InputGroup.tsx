981b27cdc35b3082cd03fc9ca036e40df1322ba5
sangmin <xectler@gmail.com>
2022-12-17 23:25:18 +0900

    update

--- client/src/components/InputGroup.tsx
+++ client/src/components/InputGroup.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import cls from 'classnames'
 
 interface InputGroupProps {
     className?: string// ? 는 옵션의 의미 있을수도 없을수도 있다. 
@@ -22,8 +23,12 @@ const InputGroup: React.FC<InputGroupProps> = ({
             <input 
                 type={type}
                 style={ { minWidth: 300 } }
-                className='w-full p-3 transition duratioin-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white'
+                className={cls(`w-full p-3 transition duratioin-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`, {'border-red-500': error})}
+                placeholder={placeholder}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
             />
+            <small className='font-medium text-red-500'>{ error }</small>
         </div>
     )
 }
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,18 +1,65 @@
-import React from 'react'
+import Link from 'next/link'
+import React, { FormEvent, useState } from 'react'
+import InputGroup from '../components/InputGroup'
+import axios from 'axios'
+import { useRouter } from 'next/router'
 
-const register = () => {
+const Register = () => {
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState<any>({})
+
+  const router = useRouter()
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+    
+    try {
+      const res = await axios.post('auth/register', {
+        email,
+        username,
+        password
+      })
+
+      console.log(res)
+      router.push('login')
+    } catch (error: any) {
+      console.log(error)
+      setErrors(error.response.data || {})
+    }
+  }
+
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}>
+            <InputGroup 
+              placeholder='email'
+              value={email}
+              setValue={setEmail}
+              error={errors.email}
+            />
+            <InputGroup 
+              placeholder='username'
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup 
+              placeholder='password'
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+            />
             <button>회원가입</button>
         </form>
         <small>
             <Link href="/login">
-                <a>로그인</a>
+              로그인
             </Link>
         </small>
     </div>
   )
 }
 
-export default register
+export default Register
