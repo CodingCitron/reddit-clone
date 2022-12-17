ea38cb1046958ac883dc7a3dc0adc3d77f193566
sangmin <xectler@gmail.com>
2022-12-17 23:25:52 +0900

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,30 @@
 import express from 'express'
 import morgan from 'morgan'
 import { AppDataSource } from './data-source'
+import authRoute from './routes/auth'
+import cors from 'cors'
 
 const app = express()
+const whitelist = ['http://192.168.50.224:3000', 'http://localhost:3000']
+const corsOptions = {
+	origin: function(origin: any, callback: Function) {
+		if(whitelist.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not Allowed Origin'))
+		}
+	}
+}
+
+app.use(
+	cors(corsOptions)
+)
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.get('/', (_, res) => res.send('running'))
+
+app.use('api/auth', authRoute)
 
 let port = 4000
 
